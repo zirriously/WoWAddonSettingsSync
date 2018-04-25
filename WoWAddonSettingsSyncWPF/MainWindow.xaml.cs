9ed97b48fc76ba137de7c289947660d886536454
Piotr Nosalik <piotr.nosalik@sophos.com>
2018-04-25 14:48:38 +0100

    Fix build errors

--- WoWAddonSettingsSyncWPF/MainWindow.xaml.cs
+++ WoWAddonSettingsSyncWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WoWAddonSettingsSyncWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
